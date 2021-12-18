976665c2ef257ef03271a65ab80548fbb1ee6753
patrickryan <theyoungsoul@gmail.com>
2021-12-18 13:36:36 -0600

    when leaving the ctx manager, if in simulator make sure to call land so the drone is not left hanging

--- examples/test-rc-control.py
+++ examples/test-rc-control.py
@@ -1,15 +1,14 @@
 from DroneBlocksTelloSimulator.DroneBlocksSimulatorContextManager import DroneBlocksSimulatorContextManager
-
+import time
 if __name__ == '__main__':
     sim_key = '4cca2720-5b26-4e90-bcdd-7662950960b4'
     # sim_key = None
     distance = 40
     with DroneBlocksSimulatorContextManager(simulator_key=sim_key) as drone:
         drone.takeoff()
-        drone.fly_forward(distance, 'in')
-        drone.fly_left(distance, 'in')
-        drone.fly_backward(distance, 'in')
-        drone.fly_right(distance, 'in')
+        drone.send_rc_control(0,0,50, 0)
+        time.sleep(3)
+        drone.send_rc_control(0,0,0,0)
         drone.land()
 
 
